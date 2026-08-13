3029b3adc1afe8bb1c28725a5091c9436c36e1dc
Krish  Kumar Suthar <krishsuthar300@gmail.com>
2026-08-13 20:58:23 +0530

    Add files via upload

---
+++ run_two_sum.py
@@ -0,0 +1,15 @@
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    return []
+
+if __name__ == '__main__':
+    nums = [2, 7, 11, 15]
+    target = 9
+    res = two_sum(nums, target)
+    if res:
+        print(f"{res}")
+    else:
+        print("No solution")
