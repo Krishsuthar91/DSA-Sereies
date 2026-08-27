e6a6dc0f55f5e2a95d6ddad2f006934a8b69fc92
Krish  Kumar Suthar <krishsuthar300@gmail.com>
2026-08-27 20:14:24 +0530

    Add files via upload

---
+++ Day-15/ClimbingStairs.py
@@ -0,0 +1,29 @@
+class Solution:
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        first = 1
+        second = 2
+
+        for i in range(3, n + 1):
+            third = first + second
+            first = second
+            second = third
+
+        return second
+
+
+class Solution:
+    def climbStairs(self, n):
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
