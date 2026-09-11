292e7af0aba9b1f0da6a18e860692b142521ec99
Krish  Kumar Suthar <krishsuthar300@gmail.com>
2026-09-11 11:32:26 +0530

    Add files via upload

---
+++ Day-30/ValidPalindrome.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s):
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+
+#Alternative Solution (Using Extra Space)
+class Solution:
+    def isPalindrome(self, s):
+        cleaned = ""
+
+        for ch in s:
+            if ch.isalnum():
+                cleaned += ch.lower()
+
+        return cleaned == cleaned[::-1]
