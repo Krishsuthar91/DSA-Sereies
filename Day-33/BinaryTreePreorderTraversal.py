9cfcd34ca6fe3de686f4a5bdfff2d813fade444d
Krish  Kumar Suthar <krishsuthar300@gmail.com>
2026-09-14 19:58:22 +0530

    Add files via upload

---
+++ Day-33/BinaryTreePreorderTraversal.py
@@ -0,0 +1,53 @@
+#Recursive
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def preorderTraversal(self, root):
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            result.append(node.val)
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return result
+
+
+
+
+#Iterative
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def preorderTraversal(self, root):
+        if not root:
+            return []
+
+        stack = [root]
+        result = []
+
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+
+            if node.right:
+                stack.append(node.right)
+
+            if node.left:
+                stack.append(node.left)
+
+        return result
