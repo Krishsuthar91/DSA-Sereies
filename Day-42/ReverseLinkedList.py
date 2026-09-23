8fe8c4bc3c8ba0a3bc8838d9ace4724d64c85095
Krish  Kumar Suthar <krishsuthar300@gmail.com>
2026-09-23 20:11:19 +0530

    Add files via upload

---
+++ Day-42/ReverseLinkedList.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev
+
+
+
+
+#Recursive Solution
+class Solution:
+    def reverseList(self, head):
+        if not head or not head.next:
+            return head
+
+        new_head = self.reverseList(head.next)
+
+        head.next.next = head
+        head.next = None
+
+        return new_head
